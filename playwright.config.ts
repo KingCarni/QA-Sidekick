789684c7e56f043e8125b72e64f18a917cbd7bcf
top-secret-games-admin <harleydean17@gmail.com>
2026-05-06 04:41:04 -0700

    QA-78 test 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env.e2e" });
+dotenv.config({ path: ".env.local" });
+
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
